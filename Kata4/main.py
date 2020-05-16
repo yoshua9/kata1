d65ddf24bbdc9b3fd41fb32cf2abdcb693505ddb
Yoshua <yoshua10@hotmail.com>
2020-05-16 14:02:41 +0200

    pygame

--- Kata4/main.py
+++ Kata4/main.py
@@ -1,1 +1,19 @@
-print('Hola, Geeks')
+import pygame
+
+screen_width = 1280
+screen_height = 960
+
+# Colors
+back_color = (200,200,200)
+
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+while True:
+    pygame.display.flip(back_color)
+
+    pygame.display.flip()
+    clock.tick(60)

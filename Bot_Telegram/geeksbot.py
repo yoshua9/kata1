4f08ed692ed40f88669c532117be1bbe76fbb75b
Yoshua <yoshua10@hotmail.com>
2020-05-16 13:45:28 +0200

    bot para telegram

--- Bot_Telegram/geeksbot.py
+++ Bot_Telegram/geeksbot.py
@@ -1,4 +1,34 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 
 def main():
-    
+    #instanciar el updater
+    updater = Updater(token=open("./Bot_Telegram/bot_token").read(), use_context=True)
+
+    #Añadir un manejador del comando Start
+    updater.dispatcher.add_handler(CommandHandler("start",start))
+
+    #Añadir un manejador del comando Repite
+    updater.dispatcher.add_handler(CommandHandler("repite",repite))
+
+    #Añadir un manejador del comando Suma
+    updater.dispatcher.add_handler(CommandHandler("suma",suma))
+
+    #solicitamos notificaciones
+    updater.start_polling()
+
+    #Capturar señales de parada
+    updater.idle()
+
+def start(update, context):
+    update.message.reply_text("Hola, soy un bot")
+
+def repite(update, context):
+    update.message.reply_text(update.message.text)
+
+def suma(update, context):
+    #args = [2,2] "Los args son de tipo str por defecto"
+    resultado = int(context.args[0]) + int(context.args[1])
+    resultado = str(resultado)
+    update.message.reply_text("El resultado es: " + resultado)
+
+main()
